4573b7ea0228b132f187298d8e16e0b3cbf87257
Gerald Emanuel Vargas Morales <202405633@est.umss.edu>
2026-05-20 15:05:35 -0400

    diseño añadido en skills

--- front/src/components/molecules/skills/CardSkills.jsx
+++ front/src/components/molecules/skills/CardSkills.jsx
@@ -9,16 +9,17 @@ const CardSkill = () => {
       {
       [...cardsData, ...cardsData].map((cardData) => {
         return (
-          <div className="skills__caja caja__dark">
-            <div className="img">
+          <div className="skills__caja caja__dark brutalist-button button-1">
+            <div className="openai-logo">
               <img
-                className={`skills__icono skills__icono__dark ${cardData.clas}`}
+                className={`skills__icono skills__icono__dark openai-icon ${cardData.clas}`}
                 src={cardData.imgSrc}
                 alt={cardData.imgAlt}
               />
-            </div>
-            <div className="skills__texto">
-              <p className="text head titulo__dark">{cardData.title}</p>
+              <div className="button-text">
+                <span>Skill</span>
+                <span>{cardData.title}</span>
+              </div>
             </div>
           </div>
         );
