2ffcfb9d59ee5dce6dc5b0121e51a9825c85b2de
Gerald Emanuel Vargas Morales <202405633@est.umss.edu>
2026-05-20 15:21:30 -0400

    diseño de burbuja añadido en hero

--- front/src/components/molecules/skills/CardSkills.jsx
+++ front/src/components/molecules/skills/CardSkills.jsx
@@ -9,17 +9,17 @@ const CardSkill = () => {
       {
       [...cardsData, ...cardsData].map((cardData) => {
         return (
-          <div className="skills__caja caja__dark brutalist-button button-1">
+          <div className="skills__caja caja__dark brutalist-button openai">
             <div className="openai-logo">
               <img
                 className={`skills__icono skills__icono__dark openai-icon ${cardData.clas}`}
                 src={cardData.imgSrc}
                 alt={cardData.imgAlt}
               />
-              <div className="button-text">
-                <span>Skill</span>
-                <span>{cardData.title}</span>
-              </div>
+            </div>
+            <div className="button-text">
+              <span>Skill</span>
+              <span>{cardData.title}</span>
             </div>
           </div>
         );
